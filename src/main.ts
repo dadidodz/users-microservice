79d945364488b94629348051262597cb0fcfc19c
Dorian <dorian.blatiere@hotmail.fr>
2026-02-11 12:02:42 +0000

    fix

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     //   await app.listen(process.env.PORT ?? 3000);
 
     app.enableCors({
-        origin: ['http://localhost:3000', 'http://192.168.0.78:3000'],
+        origin: ['http://localhost:3000', 'http://192.168.0.78:3000', 'http://192.168.27.78:3000'],
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
